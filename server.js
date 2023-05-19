b3487443d61b1c441723b3b152171cd6925dc28d
Jaaved Dewan <jilred15@gmail.com>
2023-05-19 01:27:40 -0400

    Updated server.js

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ var app = express();
 
 // setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Jaaved Dewan - 126045178");
+    res.send("My favorite movie in 2023 is Mario Bros.")
 });
 
 // setup http server to listen on HTTP_PORT
