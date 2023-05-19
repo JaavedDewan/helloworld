04bc15b0881393303eb9915c099fc1ed73c64e10
Jaaved Dewan <jilred15@gmail.com>
2023-05-19 01:38:14 -0400

    updated ouput - 2

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var app = express();
 
 // setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
-    var output = "Jaaved Dewan - 126045178\nMy favorite movie of 2023 is Mario Bros.";
+    var output = "Jaaved Dewan - 126045178<br>My favorite movie of 2023 is Mario Bros.";
     res.send(output);
 });
 
